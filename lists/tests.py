87bf1a6850b76c2061b61280484f6852256f59c4
jijunqing <jijunqing@tal.com>
2021-01-18 10:21:41 +0800

    TDD：添加了用户清单隔离的view函数，并通过了原有的单元测试和功能测试case

--- lists/tests.py
+++ lists/tests.py
@@ -1,33 +1,14 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
-from lists.views import home_page
+from lists.views import home_page,view_list
 from lists.models import Item
 # Create your tests here.
-class SmokeTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found=resolve("/")
-        self.assertEqual(found.func,home_page)
-    def test_home_page_return_correct_html(self):
-        request=HttpRequest()
-        reponse=home_page(request)
-        html=reponse.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.strip().endswith('</html>'))
-    def test_home_page_returns_correct_html(self):
-        # request=HttpRequest()
-        # response=home_page(request)
-        # html=response.content.decode('utf8')
-        # expected_html=render_to_string('home.html')
-        # self.assertEqual(html,expected_html)
-        #同时完成了root_url_resolves_to_home_page_view和home_page_return_correct_html
-        response=self.client.get('/')
-        html=response.content.decode('utf8')
-        self.assertTemplateUsed(response,'home.html')
 class HomePageTest(TestCase):
     def test_resolve_home_page(self):
         response=self.client.get('/')
+        found=resolve('/')
+        self.assertEqual(found.func,home_page)
         self.assertTemplateUsed(response,'home.html')
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
@@ -59,6 +40,11 @@ class ItemModelTest(TestCase):
         self.assertEqual(response.status_code,302)
         self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
 class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        found=resolve('/lists/the-only-list-in-the-world/')
+        response=self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertEqual(found.func,view_list)
+        self.assertTemplateUsed(response,'list.html')
     def test_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
