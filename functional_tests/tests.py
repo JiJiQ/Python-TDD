6c6c506b16fb6409eb2016d514e0b1e7beb2277e
jijunqing <jijunqing@tal.com>
2021-01-18 15:41:29 +0800

    TDD：使用分层路由，去除了功能测试用例中的强制失败提醒

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -41,7 +41,6 @@ class NewViewerTest(LiveServerTestCase):
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("2:Use peacock feathers to make a fly")
-        self.fail('Finish the test!')
     def test_can_start_a_list_for_one_user(self):
         self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
@@ -57,7 +56,6 @@ class NewViewerTest(LiveServerTestCase):
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("2:Use peacock feathers to make a fly")
-        self.fail('Finish the test!')
     def test_multiple_users_can_start_lists_at_different_urls(self):
         self.browser.get(self.live_server_url)
         inputbox=self.browser.find_element_by_id('id_new_item')
