8c0faa5f90558ef138145ae329802a340d15944b
jijunqing <jijunqing@tal.com>
2021-01-20 18:23:51 +0800

    TDD：新增空item验证功能测试case

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -18,11 +18,6 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = self.browser.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
@@ -31,6 +26,16 @@ class FunctionalTest(StaticLiveServerTestCase):
                 rows = table.find_elements_by_tag_name('tr')
                 self.assertIn(row_text, [row.text for row in rows])
                 return
+            except(AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
             except(AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
