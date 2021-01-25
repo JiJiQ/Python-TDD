52457a16e56487755f65bd29681239c5022abcb7
jijunqing <jijunqing@tal.com>
2021-01-25 11:45:16 +0800

    TDD-找回secret-key

--- TDD/settings.py
+++ TDD/settings.py
@@ -21,6 +21,7 @@ BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'lt&@-2ykr#6c)wze850i7!fwi1wq*s7!vg6c7+cj3kuf0#(oav'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
