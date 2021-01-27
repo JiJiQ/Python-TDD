cb25933380866becdfb998f533cdc679a5b54ad8
jijunqing <jijunqing@tal.com>
2021-01-27 18:32:59 +0800

    TDD-测试fabric

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -37,7 +37,7 @@ def _update_settings(source_folder,site_name):
         chars='abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key=''.join(random.SystemRandom().choice(chars) for _ in range(50))
         append(secret_ksy_file,f'SECRET_KEY="{key}"')
-    append(settings_path,'\nfrom .secret_key import SECRET_KEY',escape=False)
+    append(settings_path,'from .secret_key import SECRET_KEY')
 def _update_virtualenv(source_folder):
     virtualenv_folder=source_folder+'/../virtualenv'
     if not exists(virtualenv_folder+'/bin/pip'):
