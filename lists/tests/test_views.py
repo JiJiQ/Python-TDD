2993b0b9e464cb07dc1b91cba2e4d34dc1f9e3a6
jijunqing <jijunqing@tal.com>
2021-01-27 19:04:57 +0800

    TDD：通过单元测试和回归功能测试——用户my_lists模板和视图

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,7 +4,10 @@ from django.utils.html import escape
 from lists.views import home_page
 from lists.models import Item,List
 from lists.forms import ItemForm,ExistingListItemForm,EMPTY_ITEM_ERROR,DUPLICATE_ITEM_ERROR
+from django.contrib.auth import get_user_model
 # Create your tests here.
+
+User=get_user_model()
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
@@ -116,6 +119,12 @@ class NewListTest(TestCase):
         self.assertIsInstance(response.context['form'],ItemForm)
 class MyListsTest(TestCase):
     def test_my_lists_url_renders_my_lists_template(self):
-        response=self.client.get('/lists/users/a@b.com/')
+        User.objects.create(email='a@b.com')
+        response=self.client.get('/lists/user/a@b.com/')
         self.assertTemplateUsed(response,'my_lists.html')
+    def test_passes_correct_owner_to_template(self):
+        User.objects.create(email='wrong@owner.com')
+        correct_user=User.objects.create(email='a@b.com')
+        response=self.client.get('/lists/user/a@b.com/')
+        self.assertEqual(response.context['owner'],correct_user)
 
--- lists/urls.py
+++ lists/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^new$',views.new_lists,name='new_list'),
     url(r'^(\d+)/$',views.view_lists,name='view_list'),
-    url(r'^users/(.+)/$',views.my_lists,name='my_lists'),
+    url(r'^user/(.+)/$',views.my_lists,name='my_lists'),
 ]
