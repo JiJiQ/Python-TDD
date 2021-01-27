f18668680afef81b1b0856469522d0b6ddcdb1f2
jijunqing <jijunqing@tal.com>
2021-01-27 19:26:57 +0800

    TDD：所有单元测试和功能厕所通过——用户的my_lists功能，且list以第一个item命名

--- lists/models.py
+++ lists/models.py
@@ -7,6 +7,9 @@ class List(models.Model):
     owner=models.ForeignKey(settings.AUTH_USER_MODEL,blank=True,null=True,on_delete=models.CASCADE)
     def get_absolute_url(self):
         return reverse('view_list',args=[self.id])
+    @property
+    def name(self):
+        return self.item_set.first().text
 class Item(models.Model):
     text=models.TextField(default="")
     list=models.ForeignKey(List,default=None,on_delete=models.CASCADE)
